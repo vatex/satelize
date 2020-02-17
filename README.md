b89221945ab77137a15c314561f2d13ffadaf62a
Valeri Kuparashvili <valeri.developer@gmail.com>
2020-02-17 23:04:25 +0400

    updated db

--- README.md
+++ README.md
@@ -1,17 +1,5 @@
 **Satelize** NodeJS module to retrieve user location information based on IP, combined with expressjs for instance make life easier to get some stuff as latitude/longitude of your visitor.
 
-Inspired and using http://www.telize.com/ service. ~~Free today~~.
-
-As API is not free anymore, it uses a small dataset (provided by maxmind) to fetch information about IP requested, it won't work for all cases.
-
-## Why ?
-
-Because ecchymose in the nose. I needed something but on server side.
-
-With expressjs for instance, you can get your request IP, then just need to use this library that will make the call to get user location data.
-
-And it is done.
-
 ## Install
 
 ~~~
@@ -21,7 +9,7 @@ npm install satelize-lts
 ## Usage
 
 ```javascript
-var satelize = require('satelize-lts');
+const satelize = require('satelize-lts');
 
 // Example retrieve IP from request
 // var ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
@@ -71,14 +59,3 @@ Details
 
 - **ip** : IPV4 better.
 - **timeout** : default 1000ms
-
-## License
-
-MIT License
-Copyright (c) 2017 Valeri Kuparashvili
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- src/satelize.js
+++ src/satelize.js
@@ -1,83 +1,43 @@
-// DEPENDENCIES
-
-var path = require('path');
-var http = require('http');
-
-// db mode
-var mmdbreader = require('maxmind-db-reader');
-
-// CODE
-
-
-/* SERVICE PROVIDED BY TELIZE.COM http://www.telize.com/geoip/46.19.37.108 */
-
-var serviceHost = 'www.telize.com';
-var servicePath = '/geoip';
-var serviceJSONP = '?callback=getgeoip';
+const path = require('path');
+const mmdbreader = require('maxmind-db-reader');
 
 function Satelize() {
-  this.init();
+    this.init();
 }
 
-Satelize.prototype.init = function() {
-
-  //  load db
-  this.db = mmdbreader.openSync(path.join(__dirname,'/DB/20171003/GeoLite2-City.mmdb'));
-
-  this.initialized = true;
+Satelize.prototype.init = () => {
+    this.db = mmdbreader.openSync(path.join(__dirname,'/DB/GeoLite2-City.mmdb'));
+    this.initialized = true;
 };
 
-Satelize.prototype.satelize = function(options, next) {
-  if (!this.initialized) {
-    return next(new Error('db not loaded yet'));
-  }
-
-  var data = this.db.getGeoDataSync(options.ip);
-
-  if (data) {
-
-    if (!data.country) {
-      data.country = {
-        iso_code: null,
-        names: null
-      }
+Satelize.prototype.satelize = (options, next) => {
+    if (!this.initialized) {
+        return next(new Error('db not loaded yet'));
     }
 
-    return next(null, {
-      ip: options.ip,
-      continent_code: data.continent.code,
-      continent: data.continent.names,
-      country_code: data.country.iso_code,
-      country: data.country.names,
-      latitude: data.location.latitude,
-      longitude: data.location.longitude,
-      timezone : data.location.time_zone
-    });
-  }
+    var data = this.db.getGeoDataSync(options.ip);
+    
+    if (data) {
+        if (!data.country) {
+            data.country = {
+                iso_code: null,
+                names: null
+            }
+        }
 
-  return next(null, null);
+        return next(null, {
+            ip: options.ip,
+            continent_code: data.continent.code,
+            continent: data.continent.names,
+            country_code: data.country.iso_code,
+            country: data.country.names,
+            latitude: data.location.latitude,
+            longitude: data.location.longitude,
+            timezone : data.location.time_zone
+        });
+    }
 
-  // TODO: refactor old way of to doing with user token and new API
- /* var path = (options.ip ? ('/'+options.ip) : '') + (options.JSONP ? serviceJSONP : '');
-  var timeout = options.timeout || 1000;
-  var hasTimeout = false;
-  var opts = {
-    hostname: serviceHost,
-    path: servicePath + path,
-    method: 'GET',
-    port: 80
-  };
-  var req = http.request(opts, function(res) {
-    res.setEncoding('utf8');
-  var output = '';
-    res.on('data', function (chunk) { output += chunk; });
-    res.on('end', function() { if(!hasTimeout) return next(null, output); });
-  });
-  req.on('error', function(e) {if(!hasTimeout) return next(e); });
-  req.setTimeout(timeout, function() { hasTimeout=true; return next(new Error('timeout')); });
-  req.end();*/
+    return next(null, null);
 };
-
-// EXPORTS
 
 module.exports = new Satelize();
